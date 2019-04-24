729447c6e22aab538dabef8358eb598f6c78ba88
Let's Talk Science | parlons sciences <49924668+letstalkscience@users.noreply.github.com>
2019-04-24 11:22:07 -0600

    Add files via upload

---
+++ cozir.ts
@@ -0,0 +1,104 @@
+/**
+ * makecode COZIR LTS package.
+ * Let's Talk Science - Living Space Action Project
+ * https://www.letstalkscience.ca
+ * Author: Simon Monk
+ * https://www.monkmoakes.com
+ * Co-Author: Jennifer Ferguson
+ * Date: 05-2018
+ */
+
+
+/**
+ * Let's Talk Science! COZIR block
+ */
+//% color=190 weight=100 icon="\uf135" block="Let's Talk Science!"
+namespace COZIR {
+
+    let value_str = ""
+    let co2 = 0
+    let temp = 0
+    let rh = 0
+    let response = ""
+    let altitude = 0
+
+    serial.onDataReceived(serial.delimiters(Delimiters.NewLine), () => {
+        response = serial.readUntil(serial.delimiters(Delimiters.NewLine))
+        //basic.showString(response)
+        value_str = response.substr(3, 5)
+        let value = parseInt(value_str)
+        // basic.showString(response.charAt(1))
+        if (response.charAt(1) == 'Z') {
+            let co2_uncompensated = value
+            co2 = co2_uncompensated + (altitude * 556) / 10000
+        }
+        if (response.charAt(1) == 'T') {
+            temp = (value - 1000) / 10
+        }
+        if (response.charAt(1) == 'H') {
+            rh = value / 10
+        }
+    })
+
+    /**
+    * Return the CO2 concentration in parts per million (ppm)
+    */
+    //% blockId=device_co2_toggle block="CO2 (PPM)"
+    export function Co2(): number {
+        serial.writeString("Z\r\n")
+        basic.pause(200)
+        return co2
+    }
+
+    /**
+    * Return the temperature in degrees Centigrade
+    */
+    //% blockId=device_temperature_toggle block="Temperature (°C)"
+    export function temperature(): number {
+        serial.writeString("T\r\n")
+        basic.pause(200)
+        return temp
+    }
+
+    /**
+    * Return the relative humidty as a percentage
+    */
+    //% blockId=device_relativeHumidity_toggle block="Relative Humidity (RH)"
+    export function relativeHumidity(): number {
+        serial.writeString("H\r\n")
+        basic.pause(200)
+        return rh
+    }
+
+    /**
+    * Run this block when in fresh air and the module will re-calibrate it
+    * assuming that the CO2 level is 400ppm
+    */
+    //% blockId=device_calibrateCo2_toggle block="Calibrate CO2"
+    //% advanced=true
+    export function calibrateCo2(): void {
+        serial.writeString("G\r\n")
+    }
+
+    /**
+    * Compensate for altitude. Specify altitude in metres
+    * @param metres altitude in metres
+    */
+    //% blockId=device_setAltitude_toggle block="Set Altitude"
+    //% advanced=true
+    export function setAltitude(metres: number): void {
+        altitude = metres
+    }
+
+    /**
+    * Setup the COZIR module -only used during manufacturing
+    * this sets the mode of the COZIR to 'request'
+    */
+    //% blockId=device_setupCozir_toggle block="Set Up COZIR"
+    //% advanced=true
+    export function setupCozir(): void {
+        serial.writeString("K 2\r\n")
+        basic.pause(300)
+    }
+
+} 
