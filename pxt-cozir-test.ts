5947f4b38d61053a503e68cbd186ce8c660e76ca
Let's Talk Science | parlons sciences <49924668+letstalkscience@users.noreply.github.com>
2019-04-23 18:37:44 -0600

    Create pxt-cozir-test.ts

---
+++ pxt-cozir-test.ts
@@ -0,0 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(Math.round(COZIR.Co2()))
+    basic.showString(" PPM")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(Math.round(COZIR.relativeHumidity()))
+    basic.showString(" %RH")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(Math.round(COZIR.temperature()))
+    basic.showString(" C")
+})
+serial.redirect(
+SerialPin.P0,
+SerialPin.P1,
+BaudRate.BaudRate9600
+)
