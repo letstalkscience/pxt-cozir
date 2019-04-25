3879c7981dfa6061741698cc05088db505c2e112
Let's Talk Science | Parlons sciences <49924668+letstalkscience@users.noreply.github.com>
2019-04-25 13:52:51 -0600

    Update calibrate.ts

--- calibrate.ts
+++ calibrate.ts
@@ -7,24 +7,27 @@ input.onButtonPressed(Button.A, function () {
     }
     basic.showString(modes[mode])
 })
-input.onButtonPressed(Button.AB, function () {
-    COZIR.setupCozir()
-    basic.showString("x")
-})
 input.onButtonPressed(Button.B, function () {
     COZIR.calibrateCo2()
     basic.showString("+")
+    basic.pause(500)
+})
+input.onButtonPressed(Button.AB, function () {
+    COZIR.setAltitude(0)
+    COZIR.setupCozir()
+    basic.showString("x")
+    basic.pause(500)
 })
 serial.redirect(
 SerialPin.P0,
 SerialPin.P1,
 BaudRate.BaudRate9600
 )
+mode = 0
+modes = [" PPM", " C", " %RH"]
 basic.showIcon(IconNames.Yes)
 basic.pause(3000)
 basic.showIcon(IconNames.Happy)
-mode = 0
-modes = [" PPM", " C", " %RH"]
 basic.forever(function () {
     basic.showString(modes[mode])
     if (mode == 0) {
